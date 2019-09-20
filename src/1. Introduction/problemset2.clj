03affd55997858028d2a002b8b3f26247ab7f6ae
carlos-cecnn12 <a01373264@gmail.com>
2019-09-19 23:12:45 -0500

    exam + 3 problems missing

--- src/1. Introduction/problemset2.clj
+++ src/1. Introduction/problemset2.clj
@@ -1,4 +1,10 @@
-; Problem set 2
+;----------------------------------------------------------
+; Problem Set #1
+; Date: September 20, 2019.
+; Authors:
+;          A01373264 Pepper Pots
+;          A01160611 Anthony Stark
+;----------------------------------------------------------
 
 (require '[clojure.test :refer [deftest is run-tests]])
 (require '[clojure.math.numeric-tower :refer [abs]])
@@ -59,6 +65,19 @@
     a
     (gcd b (mod a b))))
 
+(defn insert-everywhere
+  [x lst]
+  (if (empty? lst)
+    (list x)
+    (loop [lst lst
+           tmp ()
+           i 0]
+      (= (count lst) i)
+      tmp
+      (recur (inc i) (concat tmp (list (first (split-at i lst)) (list x) (first (rest (split-at i lst)))))))))
+
+
+
 
 (defn pack
   [lst]
@@ -70,7 +89,20 @@
 
 (defn encode
   [lst]
-  )
+  (map #(vector (count %) (first %)) (pack lst)))
+
+(defn encode-modified
+  [lst]
+  (map #(if (= 1 (first %))
+          (second %)
+          %)(encode lst)))
+
+(defn decode
+  [lst]
+  (mapcat #(if (vector? %)
+             (repeat (first %) (second %))
+             (list %)) lst))
+
 
 (deftest test-insert
   (is (= '(14) (insert 14 ())))
@@ -119,7 +151,7 @@
   (is (= 12 (gcd 48 180)))
   (is (= 14 (gcd 42 56))))
 
-"(deftest test-insert-everywhere
+(deftest test-insert-everywhere
     (is (= '((1)) (insert-everywhere 1 ())))
     (is (= '((1 a) (a 1)) (insert-everywhere 1 '(a))))
     (is (= '((1 a b c) (a 1 b c) (a b 1 c) (a b c 1))
@@ -142,7 +174,8 @@
              (1 2 3 4 5 6 7 8 x 9 10)
              (1 2 3 4 5 6 7 8 9 x 10)
              (1 2 3 4 5 6 7 8 9 10 x))
-           (insert-everywhere 'x '(1 2 3 4 5 6 7 8 9 10)))))"
+           (insert-everywhere 'x '(1 2 3 4 5 6 7 8 9 10)))))
+
 
 (deftest test-pack
   (is (= () (pack ())))
@@ -150,5 +183,34 @@
          (pack '(a a a a b c c a a d e e e e))))
   (is (= '((1) (2) (3) (4) (5)) (pack '(1 2 3 4 5))))
   (is (= '((9 9 9 9 9 9 9 9 9)) (pack '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-compress
+  (is (= () (compress ())))
+  (is (= '(a b c d) (compress '(a b c d))))
+  (is (= '(a b c a d e)
+         (compress '(a a a a b c c a a d e e e e))))
+  (is (= '(a) (compress '(a a a a a a a a a a)))))
+
+(deftest test-encode
+  (is (= () (encode ())))
+  (is (= '([4 a] [1 b] [2 c] [2 a] [1 d] [4 e])
+         (encode '(a a a a b c c a a d e e e e))))
+  (is (= '([1 1] [1 2] [1 3] [1 4] [1 5])
+         (encode '(1 2 3 4 5))))
+  (is (= '([9 9]) (encode '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-encode-modified
+  (is (= () (encode-modified ())))
+  (is (= '([4 a] b [2 c] [2 a] d [4 e])
+         (encode-modified '(a a a a b c c a a d e e e e))))
+  (is (= '(1 2 3 4 5) (encode-modified '(1 2 3 4 5))))
+  (is (= '([9 9]) (encode-modified '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-decode
+  (is (= () (decode ())))
+  (is (= '(a a a a b c c a a d e e e e)
+         (decode '([4 a] b [2 c] [2 a] d [4 e]))))
+  (is (= '(1 2 3 4 5) (decode '(1 2 3 4 5))))
+  (is (= '(9 9 9 9 9 9 9 9 9) (decode '([9 9])))))
 
 (run-tests)
