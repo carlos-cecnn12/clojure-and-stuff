bab458adac99c141385c5ec0d2e26163341c1c8a
marinaharo <a01373471@itesm.mx>
2019-09-20 20:30:52 -0500

    Yasta completa

--- src/1. Introduction/problemset2.clj
+++ src/1. Introduction/problemset2.clj
@@ -144,12 +144,12 @@
              (list %))
           lst))
 
-
-(deftest test-insert
-  (is (= '(14) (insert 14 ())))
-  (is (= '(4 5 6 7 8) (insert 4 '(5 6 7 8))))
-  (is (= '(1 3 5 6 7 9 16) (insert 5 '(1 3 6 7 9 16))))
-  (is (= '(1 5 6 10) (insert 10 '(1 5 6)))))
+(deftest test-replic
+  (is (= () (replic 7 ())))
+  (is (= () (replic 0 '(a b c))))
+  (is (= '(a a a) (replic 3 '(a))))
+  (is (= '(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4)
+         (replic 4 '(1 2 3 4)))))
 
 (deftest test-expand
   (is (= () (expand ())))
@@ -158,12 +158,17 @@
   (is (= '(a b b c c c d d d d e e e e e)
          (expand '(a b c d e)))))
 
-(deftest test-replic
-  (is (= () (replic 7 ())))
-  (is (= () (replic 0 '(a b c))))
-  (is (= '(a a a) (replic 3 '(a))))
-  (is (= '(1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4)
-         (replic 4 '(1 2 3 4)))))
+(deftest test-insert
+  (is (= '(14) (insert 14 ())))
+  (is (= '(4 5 6 7 8) (insert 4 '(5 6 7 8))))
+  (is (= '(1 3 5 6 7 9 16) (insert 5 '(1 3 6 7 9 16))))
+  (is (= '(1 5 6 10) (insert 10 '(1 5 6)))))
+
+(deftest test-my-sort
+  (is (= () (my-sort ())))
+  (is (= '(0 1 3 3 4 6 7 8 9) (my-sort '(4 3 6 8 3 0 9 1 7))))
+  (is (= '(1 2 3 4 5 6) (my-sort '(1 2 3 4 5 6))))
+  (is (= '(1 5 5 5 5 5 5) (my-sort '(5 5 5 1 5 5 5)))))
 
 (deftest test-rotate-left
   (is (= () (rotate-left 5 ())))
@@ -184,6 +189,13 @@
   (is (= '(1 1 1 1 0) (binary 30)))
   (is (= '(1 0 1 1 0 0 0 0 0 1 0 0 0 0 1 1) (binary 45123))))
 
+(deftest test-prime-factors
+  (is (= () (prime-factors 1)))
+  (is (= '(2 3) (prime-factors 6)))
+  (is (= '(2 2 2 2 2 3) (prime-factors 96)))
+  (is (= '(97) (prime-factors 97)))
+  (is (= '(2 3 3 37) (prime-factors 666))))
+
 (deftest test-gcd
   (is (= 1 (gcd 13 7919)))
   (is (= 4 (gcd 20 16)))
@@ -192,30 +204,36 @@
   (is (= 12 (gcd 48 180)))
   (is (= 14 (gcd 42 56))))
 
-"(deftest test-insert-everywhere
-    (is (= '((1)) (insert-everywhere 1 ())))
-    (is (= '((1 a) (a 1)) (insert-everywhere 1 '(a))))
-    (is (= '((1 a b c) (a 1 b c) (a b 1 c) (a b c 1))
-           (insert-everywhere 1 '(a b c))))
-    (is (= '((1 a b c d e)
-             (a 1 b c d e)
-             (a b 1 c d e)
-             (a b c 1 d e)
-             (a b c d 1 e)
-             (a b c d e 1))
-           (insert-everywhere 1 '(a b c d e))))
-    (is (= '((x 1 2 3 4 5 6 7 8 9 10)
-             (1 x 2 3 4 5 6 7 8 9 10)
-             (1 2 x 3 4 5 6 7 8 9 10)
-             (1 2 3 x 4 5 6 7 8 9 10)
-             (1 2 3 4 x 5 6 7 8 9 10)
-             (1 2 3 4 5 x 6 7 8 9 10)
-             (1 2 3 4 5 6 x 7 8 9 10)
-             (1 2 3 4 5 6 7 x 8 9 10)
-             (1 2 3 4 5 6 7 8 x 9 10)
-             (1 2 3 4 5 6 7 8 9 x 10)
-             (1 2 3 4 5 6 7 8 9 10 x))
-           (insert-everywhere 'x '(1 2 3 4 5 6 7 8 9 10)))))"
+(deftest test-insert-everywhere
+  (is (= '((1)) (insert-everywhere 1 ())))
+  (is (= '((1 a) (a 1)) (insert-everywhere 1 '(a))))
+  (is (= '((1 a b c) (a 1 b c) (a b 1 c) (a b c 1))
+         (insert-everywhere 1 '(a b c))))
+  (is (= '((1 a b c d e)
+           (a 1 b c d e)
+           (a b 1 c d e)
+           (a b c 1 d e)
+           (a b c d 1 e)
+           (a b c d e 1))
+         (insert-everywhere 1 '(a b c d e))))
+  (is (= '((x 1 2 3 4 5 6 7 8 9 10)
+           (1 x 2 3 4 5 6 7 8 9 10)
+           (1 2 x 3 4 5 6 7 8 9 10)
+           (1 2 3 x 4 5 6 7 8 9 10)
+           (1 2 3 4 x 5 6 7 8 9 10)
+           (1 2 3 4 5 x 6 7 8 9 10)
+           (1 2 3 4 5 6 x 7 8 9 10)
+           (1 2 3 4 5 6 7 x 8 9 10)
+           (1 2 3 4 5 6 7 8 x 9 10)
+           (1 2 3 4 5 6 7 8 9 x 10)
+           (1 2 3 4 5 6 7 8 9 10 x))
+         (insert-everywhere 'x '(1 2 3 4 5 6 7 8 9 10)))))
+
+(deftest test-deep-reverse
+  (is (= () (deep-reverse ())))
+  (is (= '(3 (d c b) a) (deep-reverse '(a (b c d) 3))))
+  (is (= '(((6 5) 4) 3 (2 1))
+         (deep-reverse '((1 2) 3 (4 (5 6)))))))
 
 (deftest test-pack
   (is (= () (pack ())))
@@ -223,5 +241,34 @@
          (pack '(a a a a b c c a a d e e e e))))
   (is (= '((1) (2) (3) (4) (5)) (pack '(1 2 3 4 5))))
   (is (= '((9 9 9 9 9 9 9 9 9)) (pack '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-compress
+  (is (= () (compress ())))
+  (is (= '(a b c d) (compress '(a b c d))))
+  (is (= '(a b c a d e)
+         (compress '(a a a a b c c a a d e e e e))))
+  (is (= '(a) (compress '(a a a a a a a a a a)))))
+
+(deftest test-encode
+  (is (= () (encode ())))
+  (is (= '([4 a] [1 b] [2 c] [2 a] [1 d] [4 e])
+         (encode '(a a a a b c c a a d e e e e))))
+  (is (= '([1 1] [1 2] [1 3] [1 4] [1 5])
+         (encode '(1 2 3 4 5))))
+  (is (= '([9 9]) (encode '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-encode-modified
+  (is (= () (encode-modified ())))
+  (is (= '([4 a] b [2 c] [2 a] d [4 e])
+         (encode-modified '(a a a a b c c a a d e e e e))))
+  (is (= '(1 2 3 4 5) (encode-modified '(1 2 3 4 5))))
+  (is (= '([9 9]) (encode-modified '(9 9 9 9 9 9 9 9 9)))))
+
+(deftest test-decode
+  (is (= () (decode ())))
+  (is (= '(a a a a b c c a a d e e e e)
+         (decode '([4 a] b [2 c] [2 a] d [4 e]))))
+  (is (= '(1 2 3 4 5) (decode '(1 2 3 4 5))))
+  (is (= '(9 9 9 9 9 9 9 9 9) (decode '([9 9])))))
 
 (run-tests)
